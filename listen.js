8db8f533b5aa68d579f7762a69f041770916dfee
cnorman2000 <cnorman2000@gmail.com>
2025-06-23 14:16:02 +0100

    Task 13

--- listen.js
+++ listen.js
@@ -1,9 +1,5 @@
 const app = require("./app.js");
 
-app.listen(9090, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("listening on 9090");
-  }
-});
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
